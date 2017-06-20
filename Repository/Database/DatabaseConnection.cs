eb5bd3f34c777de1af4a84b2d93a36763b82a4ff
Jurian van Woerkom <Jurian.van.woerkom@student.fontys.nl>
2017-06-20 11:59:04 +0200

    Added all needed projects

---
+++ Repository/Database/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace Repository.Database
+{
+    public class DatabaseConnection
+    {
+        private static string connectionString = @"Server=mssql.fhict.local;Database=dbi369682;User Id=dbi369682;Password=liveperformance;";
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+        }
+    }
+}
